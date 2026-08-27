345485bdc243868aa784995d0229eeb1cc950439
Lucas Caldardo <llucascalldardo@gmail.com>
2026-08-27 17:36:25 -0300

    feat: cria a classe de teste para tool calling

--- src/test/java/lucas/api/fala/OpenAiChatClientlIT.java
+++ src/test/java/lucas/api/fala/OpenAiChatClientlIT.java
@@ -1,7 +1,6 @@
 package lucas.api.fala;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-public class OpenAiChatModelIT {
+public class OpenAiChatClientlIT {
 
     @Autowired
     OpenAiChatModel openAiChatModel;
--- src/test/java/lucas/api/fala/TollCallingIT.java
+++ src/test/java/lucas/api/fala/TollCallingIT.java
@@ -1,25 +1,40 @@
 package lucas.api.fala;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-public class OpenAiChatModelIT {
+public class TollCallingIT {
 
     @Autowired
     OpenAiChatModel openAiChatModel;
 
+    static class MathTools{
+
+        @Tool(description = "Soma dois numeros inteiros")
+        public int sum(int a, int b){
+            return a + b;
+        }
+
+        @Tool(description = "Subtrai dois numeros inteiros")
+        public int diff(int a, int b){
+            return a - b;
+        }
+
+    }
 
 
     @Test
     void should_excuteSum_when_prompted(){
-        var chatClient = ChatClient.builder(openAiChatModel).defaultSystem("Você é um matematico").build();
+        var chatClient = ChatClient.builder(openAiChatModel).defaultSystem("Você é um matematico")
+                .defaultTools(new MathTools())
+                .build();
 
         var response = chatClient.prompt("Some 10 + 20. Depois subtraia 30 do resultado anterior. Exiba apenas o resultado final sem explicações mas dentro de uma caixa com formada por varios simbolos pra que fica facil de ler")
                 .call().content();
