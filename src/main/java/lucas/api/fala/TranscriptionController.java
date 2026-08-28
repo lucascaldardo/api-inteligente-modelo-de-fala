84fc05d7d069034d39ce714d391e89a4a726c88b
Lucas Caldardo <llucascalldardo@gmail.com>
2026-08-28 18:58:51 -0300

    feat: Cria o controller de transcription

---
+++ src/main/java/lucas/api/fala/TranscriptionController.java
@@ -0,0 +1,29 @@
+package lucas.api.fala;
+
+import org.springframework.ai.audio.transcription.TranscriptionModel;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.awt.*;
+
+@RestController
+@RequestMapping("/api")
+public class TranscriptionController {
+
+    private final TranscriptionModel transcriptionModel;
+
+    public TranscriptionController(TranscriptionModel transcriptionModel) {
+        this.transcriptionModel = transcriptionModel;
+    }
+
+    @PostMapping(value = "/transcribe", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    String transcribe(@RequestParam("file")MultipartFile file){
+        var resource = file.getResource();
+        return transcriptionModel.transcribe(resource);
+    }
+
+}
--- src/test/java/lucas/api/fala/OpenAiTranscriptionModelIT.java
+++ src/test/java/lucas/api/fala/OpenAiTranscriptionModelIT.java
@@ -1,13 +1,36 @@
 package lucas.api.fala;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class OpenAiTranscriptionModelIT {
 
     @Autowired
     OpenAiAudioTranscriptionModel openAiAudioTranscriptionModel;
+
+    @ParameterizedTest
+    @CsvSource({
+            "recording-1.m4a, 80",
+            "recording-2.m4a, 40",
+            "recording-3.m4a, 120",
+            "recording-4.m4a, 90",
+            "recording-5.m4a, 200",
+            "recording-6.m4a, 60",
+    })
+    public void should_containExpectedKeywords_when_audioFolesAreProcessed(String fileName, String expectedKeyword){
+        var recording = new ClassPathResource("audio/" + fileName);
+
+        var response = openAiAudioTranscriptionModel.call(recording);
+
+        System.out.println(response);
+        assertThat(response).contains(expectedKeyword);
+    }
 
 }
