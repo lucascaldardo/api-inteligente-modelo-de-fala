1fa5134011e3be25b76a6ef82066cb9f549b4ed9
Lucas Caldardo <llucascalldardo@gmail.com>
2026-08-29 08:59:01 -0300

    feat: Adiciona o controller para testar o texto to speech

---
+++ src/main/java/lucas/api/fala/TextToSpeechController.java
@@ -0,0 +1,43 @@
+package lucas.api.fala;
+
+import org.springframework.ai.audio.tts.TextToSpeechModel;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.StandardCharsets;
+
+@RestController
+@RequestMapping("/api")
+public class TextToSpeechController {
+    private final TextToSpeechModel textToSpeechModel;
+
+    public TextToSpeechController(TextToSpeechModel textToSpeechModel) {
+        this.textToSpeechModel = textToSpeechModel;
+    }
+
+    @PostMapping(value = "/sinthesize", produces = "audio/mp3")
+    public ResponseEntity<Resource> sinthesize(@RequestBody SysthesizeRequest request){
+        byte[] audio = textToSpeechModel.call(request.text());
+        var resource = new ByteArrayResource(audio);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        ContentDisposition.attachment()
+                                .filename("audio.mp3")
+                                .build()
+                                .toString())
+                .body(resource);
+
+    }
+
+    public record SysthesizeRequest(String text){
+
+    }
+}
--- src/test/java/lucas/api/fala/OpenAiSpeechModelIT.java
+++ src/test/java/lucas/api/fala/OpenAiSpeechModelIT.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.IOException;
+import java.nio.file.Files;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -18,7 +21,12 @@ public class OpenAiSpeechModelIT {
     OpenAiAudioSpeechModel openAiAudioSpeechModel;
 
     @Test
-    public void should_produceAudio_when_textIsProvided(){
+    public void should_produceAudio_when_textIsProvided() throws IOException {
+        var response = openAiAudioSpeechModel.call("O Valor total do serviço ficou 89 reais. Posso confirmar o oagamento?");
+        assertThat(response).hasSizeGreaterThan(1024);
+        var tempFile = Files.createTempFile("AUDIO", ".mp3");
+        Files.write(tempFile, response);
+        System.out.println(tempFile.toAbsolutePath());
 
 
 
